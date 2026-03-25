e7dbe6265fabb660067b97b359c3d13529fac0f7
Abril A Hernandez <66395892+AndyUnadvised@users.noreply.github.com>
2026-03-25 14:09:29 -0500

    1st file

---
+++ Algorithms_Hw3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Generate(){/*generates unique integers between 0-20000*/
+    }
+void LinearSearch(int arr[], int size, int target){/*performs linear search on the array*/}
+void BinarySearch(int arr[], int size, int target){/*performs binary search on the array*/}
